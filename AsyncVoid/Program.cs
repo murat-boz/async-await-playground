1b9ac698f6c0051455e8c528b92b44552b4c4ecd
murat-boz <muratboz@hotmail.com>
2023-03-23 01:01:53 +0300

    Add project files.

---
+++ AsyncVoid/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace AsyncVoid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            BackgroundQueue.FireAndForget(async () =>
+            {
+                await httpClient.GetAsync("http://test/api/1");
+            });
+
+            Console.ReadLine();
+        }
+    }
+
+    public class BackgroundQueue
+    {
+        /// <summary>
+        /// ❌ Bad approach
+        /// This implementation cause to crash the application
+        /// </summary>
+        /// <param name="action"></param>
+        public static void FireAndForget(Action action) 
+        { 
+            action?.Invoke();
+        }
+
+        /// <summary>
+        /// ✅ Good approach
+        /// </summary>
+        /// <param name="action"></param>
+        public static void FireAndForget(Func<Task> action)
+        {
+            action?.Invoke();
+        }
+    }
+}
