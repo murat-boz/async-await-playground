c5f1b5b53217ce8adac8320afcffa14798af4ec7
murat-boz <muratboz@hotmail.com>
2023-03-26 00:59:46 +0300

    Added awaitable pattern sample.

---
+++ AwaitablePattern/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace AwaitablePattern
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            CancellationTokenSource cts = new (10000);
+
+            Console.WriteLine("Starting...");
+
+            var sw = Stopwatch.StartNew();
+
+            await cts.Token;
+
+            Console.WriteLine($"Done - Elapsed time: {sw.Elapsed}");
+        }
+    }
+
+    public static class CancellationTokenExtensions
+    {
+        public static TaskAwaiter GetAwaiter(this CancellationToken cancellationToken)
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return Task.CompletedTask.GetAwaiter();
+            }
+
+            TaskCompletionSource taskCompletionSource = new();
+
+            cancellationToken.Register(() => taskCompletionSource.SetResult());
+
+            return taskCompletionSource.Task.GetAwaiter();
+        }
+    }
+}
